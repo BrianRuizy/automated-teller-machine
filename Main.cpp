6789899efb7364ffbe4346797cd6db7775bc747b
Brian Ruiz <brianruiz123@yahoo.com>
2019-04-03 15:51:24 -0500

    Brian - update to main

--- Main.cpp
+++ Main.cpp
@@ -1,15 +1,58 @@
-#include<iostream>
-#include<vector>
-using namespace std;
+#include <iostream>
+#include <string>
+#include "Account.h"
+#include "Checkings.h"
+#include "Savings.h"
 
-int main()
+using namespace std; 
+
+int main() 
 {
+    /* Array on objects 
+    int size = 10;
 
-	vector<string> valid;
+    string accNumber[10]; 
+    Checkings Arr[size];
+    Savings Arrr[size]; */
 
-	valid.push_back()
+    //class objects instantiation
+    Checkings checkObj; 
+    Savings savObj; 
 
+    cout << "----------------------" << endl;
+    cout << "welcome 'first_name.'" << endl;
+    cout << "----------------------" << endl;
 
-	system("pause");
+    int choice; 
+
+    cout << "What would you like to do?: " << endl;
+    cout << "1. Deposit" << endl; 
+    cout << "2. Withdraw" << endl;
+    cout << "3. Check Balance" << endl; 
+    cout << "4. Account Transfer" << endl;
+
+    cin >> choice;
+
+    switch (choice){
+
+    case 1:
+    
+        break; 
+
+    case 2: 
+
+        break;
+
+    case 3:
+
+        break;	
+      
+    case 4: 
+
+        break;
+
+    }
+
 	return 0;
+
 }
