4c3568e80c6c6c39740670e47c877457e10fee47
Brian Ruiz <brianruiz123@yahoo.com>
2019-04-04 01:51:15 -0500

    Update Savings.h

--- Header Files/Savings.h
+++ Header Files/Savings.h
@@ -1,16 +1,15 @@
 #pragma once
 #include "Account.h"
 
-class Savings : public Account
+class Savings : public Account //inheritence from base class 'account'
 {
-	double balancee;
+	private: 
+		double s_balance = 0;
 
-public:
-	void setSavingsBalance(double Balance);
-	double getSavingsBalance() const;
+	public:
+		void setSavingsBalance(double Balance);
+		double getSavingsBalance() const;
+		void setWithdraw();
+		void setDeposit();
+
 };
-
-
-
-
-
