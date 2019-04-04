c34fc6b4fd3841c8f556bd50ddf3ae35200648b1
Brian Ruiz <brianruiz123@yahoo.com>
2019-04-04 01:50:11 -0500

    updated base header file
    includes login function and credentials

--- Header Files/Account.h
+++ Header Files/Account.h
@@ -6,12 +6,18 @@ class Account
 {
 	private:
 		string firstName, lastName;
-		int accNumber;
+		string accountName;
+		int accNumber; //this accNumber may become redundant
+
+		int accountNumber = 11223344; //for login
+		int pinNumber = 1234;
+		
 
 	public:
-		void setAccountName(string firstname, string lastname);
+		void setAccountName(string firstname, string lastname); 
+		string getAccountName();
 		void setAccountNumber(int accno);
-	
+		bool login();
+
 	
 };
-
