6ed694d5aa313b464b40838540ab9e399696ccc8
Brian Ruiz <brianruiz123@yahoo.com>
2019-04-04 01:45:58 -0500

    Add files via upload

--- Main.cpp
+++ Main.cpp
@@ -1,81 +1,37 @@
 #include <iostream>
-#include <string>
-#include "Account.h"
-#include "Checkings.h"
-#include "Savings.h"
-
 using namespace std; 
 
+int main() {
 
-int mainMenu() {
+	int chose; 
 
-	int choice = 0;
+	cout << "-------------------------------" << endl;
+	cout << "Welcome to The ATM" << endl;
+	cout << "-------------------------------" << endl;
+	cout << "Choose action below:" << endl;
+	cout << "(1) Login" << endl;
+	cout << "(2) " << endl;
+	cout << "(3) Print an Account" << endl;
+	cout << "(4) Print al" << endl;
+	cout << "(5) EXIT" << endl;
+	cout << "Choose wisely: ";
+	cin >> chose;
 
-	cout << "\n-----------------------------" << endl;
-	cout << "|What would you like to do?:|" << endl;
-	cout << "-----------------------------" << endl;
-	cout << "| 1. Deposit                 |" << endl;
-	cout << "| 2. Withdraw                |" << endl;
-	cout << "| 3. Check Balance           |" << endl;
-	cout << "| 4. Account Transfer        |" << endl;
-	cout << "| 5. [Exit]                  |" << endl;
-	cout << "-----------------------------" << endl;
+	/* switch (switch_on)
+	{
+	default:
+		break;
+	case 1:
+		break; 
+	case 2: 
+		break;
+	case 3:
+		break;
+	case 4: 
+		break;
 
-	cin >> choice;
-	return choice; 
-}
-
-
-int main() 
-{
-
-	cout << "---------------------------" << endl;
-	cout << "|   welcome 'name.'       |" << endl;
-	cout << "---------------------------" << endl;
-
-	//If creating more than 1 bank accounts, 
-	//in this program we will only run with one account.
-    /* Array on objects  
-    int size = 10;
-    string accNumber[10]; 
-    Checkings Arr[size];
-    Savings Arrr[size]; */
-
-    //class objects instantiation
-    Checkings checkObj; 
-    Savings savObj; 
-
-	bool isNotFinished = true;
-
-	do {
-
-		switch (mainMenu()) {
-
-			case 1:
-				break;
-
-			case 2:
-				break;
-
-			case 3:
-				break;
-
-			case 4:
-				break;
-
-			case 5:
-				cout << "Thank you for Choosing Chase Fargo of America." << endl;
-				break;
-
-			default:
-				cout << "Invalid choice!" << endl;
-				break;
-		}
-
-	} while (isNotFinished);
-
-
-	system("pause");
+	}
+	*/
 	return 0;
 
 }
