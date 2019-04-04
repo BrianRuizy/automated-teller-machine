42e3ff2d70d455732e3723cb2ab4d1961d7132ea
Brian Ruiz <brianruiz123@yahoo.com>
2019-04-04 01:53:10 -0500

    Update Checkings.cpp

--- Source Files/Checkings.cpp
+++ Source Files/Checkings.cpp
@@ -1,14 +1,38 @@
 #include "Checkings.h"
+#include <iostream>
 
 
-
-void Checkings:: setBalance(double depositt)
+void Checkings:: setBalance(double deposit)
 {
-	balance = balance + depositt;
+	c_balance = c_balance + deposit;
 }
-
 
 double Checkings:: getBalance() const
 {
-	return balance;
+	return c_balance;
+}
+
+void Checkings::setWithdraw()
+{
+	int wdraw = 0;
+	cout << "Enter withdrawal amount: ";
+	cin >> wdraw;
+
+	if (c_balance >= wdraw) {
+		c_balance = c_balance - wdraw;
+		cout << "Your updated Checkings balance is: " << c_balance << endl;
+	}
+	else {
+		cout << "Not able to withdraw $" << wdraw << " from account.";
+		cout << "Not enough funds..." << endl;
+	}
+}
+
+void Checkings::setDeposit()
+{
+	int dep = 0;
+	cout << "Enter deposit amount: ";
+	cin >> dep; 
+	c_balance = c_balance + dep;
+	cout << "Your updated Checkings balance is: " << getBalance() << endl;
 }
