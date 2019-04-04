9bed681bacbd4ba277b9937b157c9202894ae973
Brian Ruiz <brianruiz123@yahoo.com>
2019-04-04 01:53:41 -0500

    Update Savings.cpp

--- Source Files/Savings.cpp
+++ Source Files/Savings.cpp
@@ -1,14 +1,39 @@
 #include "Savings.h"
+#include "iostream"
 
 
-
-void Savings::setSavingsBalance(double depositt)
+void Savings::setSavingsBalance(double deposit)
 {
-	balancee = balancee + depositt;
+	s_balance = s_balance + deposit;
 }
 
 
 double Savings::getSavingsBalance() const
 {
-	return balancee;
+	return s_balance;
+}
+
+void Savings::setWithdraw()
+{
+	int wdraw = 0;
+	cout << "Enter withdrawal amount: ";
+	cin >> wdraw;
+
+	if (s_balance >= wdraw) {
+		s_balance = s_balance - wdraw;
+		cout << "Your updated Checkings balance is: " << s_balance << endl;
+	}
+	else {
+		cout << "Not able to withdraw $" << wdraw << " from account." << endl;
+		cout << "Not enough funds..." << endl;
+	}
+}
+
+void Savings::setDeposit()
+{
+	int dep = 0;
+	cout << "Enter deposit amount: ";
+	cin >> dep;
+	s_balance = s_balance + dep;
+	cout << "Your updated Savings balance is: " << getSavingsBalance() << endl;
 }
