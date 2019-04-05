c56203704a9b52cfc97af2362bc756bd83071bbc
Kaleab Teka <kaleabteka@gmail.com>
2019-04-04 22:52:33 -0500

    Final Checkings.h

--- Header Files/Checkings.h
+++ Header Files/Checkings.h
@@ -9,7 +9,6 @@ class Checkings : public Account  //inheritence from base class 'account'
   public:
     void setBalance(double deposit);
     double getBalance() const;
-    double dislpayCheckingsBalance() const; // edit
     void setWithdraw();
     void setDeposit();
 	double getTransfer();
